de2d83aa366cf2aa77b3fe15528aac2642d7054c
Adhiatma <hepy.ratma@gmail.com>
2022-12-13 22:08:56 +0700

    Pengulangan dengan for dan while untuk jumlah buku tertentu, ex: 10 buku

---
+++ Dasar_sintaksis_pengulangan_dengan_for.py
@@ -0,0 +1,27 @@
+"""
+Program pengulangan membaca buku
+"""
+jumlah_buku = 10
+print("Ibu berkata, 'baca semua buku")
+
+jumlah_buku_yang_sudah_dibaca = 0
+print(f"Jumlah buku yang sudah dibaca {jumlah_buku_yang_sudah_dibaca}")
+
+print("dengan for")
+for jumlah_buku_yang_sudah_dibaca in range(1, 10):
+    print(f"buku ke {jumlah_buku_yang_sudah_dibaca} sudah dibaca")
+
+print(f'jumlah buku yang sudah dibaca {jumlah_buku_yang_sudah_dibaca}')
+
+#tanpa for
+print("tanpa for")
+print("Membaca buku ke-1")
+print("Membaca buku ke-2")
+print("Membaca buku ke-3")
+print("Membaca buku ke-4")
+print("Membaca buku ke-5")
+print("Membaca buku ke-6")
+print("Membaca buku ke-7")
+print("Membaca buku ke-8")
+print("Membaca buku ke-9")
+print("Membaca buku ke-10")
